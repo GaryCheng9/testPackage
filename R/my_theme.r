7a7918a3cb2b46964fd9de2bd949ab8f4567aba5
GaryCheng9 <garychengguo@gmail.com>
2024-03-07 19:43:57 -0500

    Test package updated

--- R/my_theme.r
+++ R/my_theme.r
@@ -1,17 +1,17 @@
-#' A function that returns my customized ggplot2 theme.
+#' A function that returns the customized ggplot2 theme.
 #'
 #' @return The customized ggplot2 theme.
 #' @export
 #'
 #' @examples
-#' ggplot(mtcars, aes(wt, mpg)) + geom_point() + my_theme()
+#' ggplot2::ggplot(mtcars, ggplot2::aes(wt, mpg)) + ggplot2::geom_point() + my_theme()
 my_theme <- function() {
-  result <- theme(
-    plot.background = element_rect(fill = "#B6D0E2"),
-    panel.background = element_rect(fill = "#B6D0E2"),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor = element_blank(),
-    axis.line.x = element_line(colour = "black")
+  result <- ggplot2::theme(
+    plot.background = ggplot2::element_rect(fill = "#B6D0E2"),
+    panel.background = ggplot2::element_rect(fill = "#B6D0E2"),
+    panel.grid.major.x = ggplot2::element_blank(),
+    panel.grid.minor = ggplot2::element_blank(),
+    axis.line.x = ggplot2::element_line(colour = "black")
   )
   return(result)
 }
